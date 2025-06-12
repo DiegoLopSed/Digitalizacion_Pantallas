5736f37cc52b1129d8146a0ed8460beff242d762
Diego Lopez Sedeño <diegolopsed160703@gmail.com>
2025-06-11 19:21:40 -0600

    Archivo de autenticación de usuarios

--- Digitalizacion-Pantallas/includes/auth.php
+++ Digitalizacion-Pantallas/includes/auth.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-
 // Verificar si la sesión del usuario está activa
 if (!isset($_SESSION['user'])) {
     header("Location: login.php"); // Redirigir al login si no hay sesión activa
