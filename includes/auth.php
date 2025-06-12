0cfde94ed3dbd9a838abeacdb3f89a64530e9bdd
Diego Lopez Sedeño <diegolopsed160703@gmail.com>
2025-06-11 18:54:33 -0600

    Se aislaron los recursos

--- includes/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Verificar si la sesión del usuario está activa
-if (!isset($_SESSION['user'])) {
-    header("Location: login.php"); // Redirigir al login si no hay sesión activa
-    exit;
-}
-?>
