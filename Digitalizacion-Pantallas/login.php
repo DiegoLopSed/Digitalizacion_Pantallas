db3ce9203820fc2de49d9174e221b2840beef0e2
Diego Lopez Sedeño <diegolopsed160703@gmail.com>
2025-06-11 19:06:58 -0600

    Archivo de logeo de usuarios

--- Digitalizacion-Pantallas/login.php
+++ Digitalizacion-Pantallas/login.php
@@ -7,7 +7,6 @@ if (isset($_SESSION['user'])) {
     header("Location: index.php");
     exit;
 }
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
